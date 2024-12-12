898920526c12cc79573cf1ac72a19f5634af699f
Doub1Bee <andy.real.men@gmail.com>
2024-12-13 01:50:36 +0300

    fixing LearningLib

---
+++ LearningLib.Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+
+using LearningLib;
+namespace LearningLib.Tests;
+
+public class Tests
+{
+
+    [Test]
+
+    public void TestAdd()
+    {
+        Assert.That(MathMethod.Add(-5, 5), Is.EqualTo(0));
+
+    }
+    [Test]
+    public void Test2()
+    {
+
+        Assert.That(MathMethod.Add(0, 0), Is.EqualTo(0));
+    }
+    [Test]
+    public void Test3()
+    {
+
+        Assert.That(MathMethod.Add(1, 9), Is.EqualTo(10));
+    }
+}
+public class Test2
+{
+    [TestCase(0, 0, 0)]
+    [TestCase(1, 1, 2)]
+    [TestCase(0, 9, 9)]
+    [TestCase(9, 0, 9)]
+
+    public void Test(int a, int b, int expected)
+    {
+        Assert.That(MathMethod.Add(a, b), Is.EqualTo(expected));
+    }
+}
---
+++ LearningLib/MathMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace LearningLib;
+
+public class MathMethod
+{
+    static public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    
+    static protected void ShowStart(int time)
+    {
+        Console.WriteLine($"Show starts in {time} minutes");
+    }
+}
