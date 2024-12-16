9bb832f9e5d507f1b8a47b6a6cc216496ffa1def
Doub1Bee <andy.real.men@gmail.com>
2024-12-17 00:10:20 +0300

    Add new methods to MyList

--- LearningLib.Tests/UnitTest1.cs
+++ LearningLib.Tests/UnitTest1.cs
@@ -10,35 +10,21 @@ public class Tests
     public void MyListAdd()
     {
         MyList l1 = new MyList(3);
-        l1.Add  ("123", 0);
+        l1.Add  ("123");
 
         Assert.That(l1.GetValue(0), Is.EqualTo("123"));
 
     }
-    [Test]
-    public void MyListDelete()
-    {
-        MyList l2 = new MyList(4);
-        l2.Add("1", 0);
-        l2.Add("2", 1);
-        l2.Add("3", 2);
-        l2.Add("4", 3);
 
-        l2.Delete(1, 2);
-
-        Assert.That(l2.GetValue(1), Is.EqualTo(null));
-        Assert.That(l2.count, Is.EqualTo(2));
-    }
-   
- 
     
- 
+    
+
     [Test]
     public void MyListGetValue()
     {
         MyList l5 = new MyList(2);
-        l5.Add("1", 0);
-        l5.Add("2", 1);
+        l5.Add("1");
+        l5.Add("2");
 
         
         Assert.That(l5.GetValue(1), Is.EqualTo("2"));
@@ -49,12 +35,23 @@ public class Tests
     public void MyListSetValue()
     {
         MyList l6 = new MyList(2);
-        l6.Add("1", 0);
-        l6.Add("2", 1);
+        l6.Add("1");
+        l6.Add("2");
 
         l6.SetValue("123", 1);
         Assert.That(l6.GetValue(1), Is.EqualTo("123"));
         Assert.That(l6.count, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void MyListCollectionInitialiser()
+    {
+        MyList mylist = new MyList(3)
+        {
+           "s1", "s2", "s3"
+        };
+        Assert.That(mylist[1], Is.EqualTo("s2"));
+        Assert.That(mylist.count, Is.EqualTo(3));
     }
 
 }
--- LearningLib/MyList.cs
+++ LearningLib/MyList.cs
@@ -1,94 +1,53 @@
 ﻿using System;
+using System.Collections;
 
-public class MyList
+public class MyList : IEnumerable
 {
-	public string[] list;
-	public int count = 0;
-	public MyList(int size)
-	{
-		list = new string[size];
-		
-	}
-	public void Add(string element, int position)
-	{
-		if (count == list.Length)
-		{
-			list = ExpandSize(list);
-			list[count] = element;
-		}
-        else
-        {
-			if (list[position] == null)
-			{
-				list[position] = element;
-			}
-			else
-			{
-				string[] tempList = new string[count + 1];
-				for (int i = 0; i < count; i++)
-				{
-					if (i < position)
-					{
-						tempList[i] = list[i];
-					}
-					else if (i == position)
-					{
-						tempList[i] = element;
-					}
-					else
-					{
-						tempList[i] = list[i+1];
-					}
-				}
-			}
-        }
-		count++;
+    public string[] list;
+    public int count = 0;
+    public MyList(int size)
+    {
+        list = new string[size];
+
     }
-	public void Delete(int position, int number)
-	{
-		while (number > 0)
-		{
-			list[position] = null;
-			number--;
-			position++;
-			count--;
-		}
-	}
-	public string GetValue(int position)
-	{ 
-		if (list != null)
-		{
-			return list[position];
-		}
-		else
-		{
-			return null;
-		}
-	
-	}
-		
-		
-	
-	public void SetValue(string setString, int position)
-	{
-		list[position] = setString;
-	}
-	public void Iterate()
-	{
-        if (list != null)
-		{
-            foreach (string str in  list)
-			{
-				Console.Write(str + " ");
-			}
-		}
-	}
-	
-	
-	public string[] ExpandSize(string[] smallList)
-	{
-		return new string[smallList.Length * 2];
-	}
-	
-	
-}
+    public void Add(string element)
+    {
+        if (count == list.Length)
+        {
+            list = ExpandSize(list);
+        }
+
+        list[count] = element;
+        count++;
+    }
+
+    public string GetValue(int position)
+    {
+        return list[position];
+    }
+
+    public void SetValue(string setString, int position)
+    {
+        list[position] = setString;
+    }
+
+
+    public string[] ExpandSize(string[] smallList)
+    {
+        return new string[smallList.Length * 2];
+    }
+
+    public string this[int index]
+    {
+        get { return list[index]; }
+    }
+   
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return list.GetEnumerator();
+    }
+    
+     
+        
+}   
